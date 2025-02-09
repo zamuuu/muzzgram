5ec19898216addbbc7bd4a5e076488743e096c38
zamuuu <samuelcorreavallejo@gmail.com>
2025-02-08 21:02:34 -0300

    Inicio terminado, se be algo en el path principal, models de usuarios creados con sus respectivos campos.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class customUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    bio = models.TextField(blank=True, null=True)
+    profile_pic = models.ImageField(
+        upload_to='static/profile_pics/', 
+        blank=True, 
+        null='True',
+        default="static/profile_pics/default_pp.jpg"
+        )
+    
+    def __str__(self):
+        return self.username
+
--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+def home(request):
+    if request.user.is_authenticated:
+        return redirect('feed') # If your are logged in, it sends it to the feed
+    return render(request, 'users/home.html') # If its not just show the login or sign up page
