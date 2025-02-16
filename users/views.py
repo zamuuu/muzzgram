2943fa95b05fbbd2c667617e0cd3c27ba0651edf
zamuuu <samuelcorreavallejo@gmail.com>
2025-02-16 17:43:35 -0300

    Login, and register template finished, with its styles

--- users/views.py
+++ users/views.py
@@ -6,3 +6,6 @@ def home(request):
     if request.user.is_authenticated:
         return redirect('feed') # If your are logged in, it sends it to the feed
     return render(request, 'users/home.html') # If its not just show the login or sign up page
+
+def register(request):
+    return render(request, 'users/register.html')
