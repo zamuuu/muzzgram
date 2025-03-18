89e7a16a33520514ea9486493184fb19f2201973
zamuuu <samuelcorreavallejo@gmail.com>
2025-03-18 17:00:55 -0300

    Evrything working atmgit log --oneline!

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
 # Create your views here.
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
