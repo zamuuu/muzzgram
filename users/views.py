0f6b1aca33535a6825a4b1a6f08e32a1d6aa3e7f
zamuuu <samuelcorreavallejo@gmail.com>
2025-03-18 16:53:53 -0300

    register functionality working

--- users/views.py
+++ users/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
 from django.contrib.auth import login
 from django.contrib import messages
 from django.contrib.auth import get_user_model
@@ -12,8 +11,11 @@ def home(request):
         return redirect('feed') # If your are logged in, it sends it to the feed
     return render(request, 'users/home.html') # If its not just show the login or sign up page
 
-User = get_user_model()
 
+def login(request):
+    pass
+
+User = get_user_model()
 def register(request):
     if request.method == "POST":
         username = request.POST.get("username")
@@ -32,3 +34,7 @@ def register(request):
         return redirect("home")
 
     return render(request, "users/register.html")
+
+@login_required
+def feed(request):
+    return render(request, "users/feed.html")
