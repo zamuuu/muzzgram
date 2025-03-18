66830c67fda7e1b8e2cb78879295e9d651f6f176
zamuuu <samuelcorreavallejo@gmail.com>
2025-03-18 16:56:24 -0300

    merged branch of implementing_register_functionality into main

--- users/views.py
+++ users/views.py
@@ -12,8 +12,24 @@ def home(request):
     return render(request, 'users/home.html') # If its not just show the login or sign up page
 
 
-def login(request):
-    pass
+def login_user(request):
+    if request.user.is_authenticated:
+        return render(request, 'users/feed.html') # If your are logged in, it sends it to the feed
+    else:
+        if request.method == "POST": 
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('feed')
+                # Redirect to a success page.
+            else:
+                messages.error(request, "Username or Password incorrect")
+                return redirect('login')
+                ...
+        else:
+            return render(request, 'users/home.html')
 
 User = get_user_model()
 def register(request):
